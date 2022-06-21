c0861622ed56acb4426e5d74c5c82d283f828625
Documenter.jl <documenter@juliadocs.github.io>
2022-06-21 18:06:26 +0000

    build based on 0a30bba

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DDMFaultSlip","category":"page"},{"location":"#DDMFaultSlip","page":"Home","title":"DDMFaultSlip","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DDMFaultSlip.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DDMFaultSlip]","category":"page"}]
+}
